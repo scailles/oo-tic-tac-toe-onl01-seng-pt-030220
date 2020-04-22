8bfd1e5f51f8b84f9cbe541e5ca8397f49f84c1f
Shaina C <scailles23@gmail.com>
2020-04-22 23:22:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,7 +100,7 @@ class TicTacToe
    def play
     @board
     turn until over?
-    draw?
+    draw? ? puts ("Cat's Game!") : nil
     won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
    
   end
